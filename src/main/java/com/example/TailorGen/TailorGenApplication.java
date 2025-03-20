15f8a04159996e9a4d41efda86db92caebdd5de6
aqsajavai10 <145002052+aqsajavai10@users.noreply.github.com>
2025-03-20 09:20:29 +0500

    Project setup

---
+++ src/main/java/com/example/TailorGen/TailorGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.TailorGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailorGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TailorGen/TailorGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TailorGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
